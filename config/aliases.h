4d89cdc76f3e626092047120773ab08adaa50a30
Brian Grant <brian@skeletonjack.net>
2025-11-22 10:09:22 -0600

    moved more stuff into a shared file

--- config/aliases.h
+++ config/aliases.h
@@ -5,18 +5,18 @@
 //#define THUMBS 30 31      33 34 // jlwffre encoder is at 32
 
 // Home row mods macro
-/*#define HRML(k1,k2,k3,k4) &hrml LCTRL k1  &hrml LALT k2  &hrml LGUI k3  &sftl LSHFT k4
-#define HRMR(k1,k2,k3,k4) &sftr RSHFT k1  &hrmr RGUI k2  &hrmr LALT k3  &hrmr RCTRL k4*/
+#define HRML(k1,k2,k3,k4) &hrml LCTRL k1  &hrml LALT k2  &hrml LGUI k3  &sftl LSHFT k4
+#define HRMR(k1,k2,k3,k4) &sftr RSHFT k1  &hrmr RGUI k2  &hrmr LALT k3  &hrmr RCTRL k4
 
 #define BACK &kp LG(LBKT)
 #define FWD &kp LG(RBKT)
 #define MISS_CTL &kp LC(UP)
 
 /* base */
-/*#define Z_HYPR &ht LC(LA(LG(LSHFT))) Z
+#define Z_HYPR &ht LC(LA(LG(LSHFT))) Z
 #define SLS_HYPR &ht LC(LA(LG(LSHFT))) FSLH
 #define B_MEH &ht LC(LA(LSHFT)) B
-#define N_MEH &ht LC(LA(LSHFT)) N*/
+#define N_MEH &ht LC(LA(LSHFT)) N
 
 /* macros */
 #define UNDO_  &kp LG(Z)
