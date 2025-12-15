a98e151b2ccad63dc1c79e91548f46f4522ef6b4
Brian Grant <brian@skeletonjack.net>
2025-12-15 16:10:35 -0600

    no, we actually want THUMBS and more of them!

--- config/behaviors.h
+++ config/behaviors.h
@@ -1,3 +1,11 @@
+// hold-trigger-key-positions configs https://zmk.dev/docs/keymaps/behaviors/hold-tap?examples=home_row_mods#positional-hold-tap-and-hold-trigger-key-positions
+// TODO these should go in behaviors.h because that's the only place that would use them
+#define KEYS_L 0 1 2 3 4  10 11 12 13 14  20 21 22 23 24
+#define KEYS_R 5 6 7 8 9  15 16 17 18 19  25 26 27 28 29
+#define THUMBS 30 31 32   33 34 35 // Corne
+//#define THUMBS 30 31      32 33 // STM Chiffre encoder is at 34
+//#define THUMBS 30 31      33 34 // jlwffre encoder is at 32
+
 / {
     behaviors {
         lt: layer_tap { // this overrides the built-in layer-tap configuration https://zmk.dev/docs/keymaps/behaviors/hold-tap#custom-hold-tap-examples
@@ -37,7 +45,7 @@
             quick-tap-ms = <175>;
 //            require-prior-idle-ms = <150>;
             bindings = <&kp>, <&kp>;
-            hold-trigger-key-positions = <KEYS_R/* THUMBS*/>;
+            hold-trigger-key-positions = <KEYS_R THUMBS>;
             hold-trigger-on-release;
         };
         hrmr: home_row_mods_right_hand {
@@ -49,7 +57,7 @@
             quick-tap-ms = <175>;
 //            require-prior-idle-ms = <150>;
             bindings = <&kp>, <&kp>;
-            hold-trigger-key-positions = <KEYS_L/* THUMBS*/>;
+            hold-trigger-key-positions = <KEYS_L THUMBS>;
             hold-trigger-on-release;
         };
 
@@ -62,7 +70,7 @@
             quick-tap-ms = <175>;
 //            require-prior-idle-ms = <150>;
             bindings = <&kp>, <&kp>;
-            hold-trigger-key-positions = <KEYS_R/* THUMBS*/>;
+            hold-trigger-key-positions = <KEYS_R THUMBS>;
             hold-trigger-on-release;
         };
         sftr: home_row_mod_shift_right_hand {
@@ -74,7 +82,7 @@
             quick-tap-ms = <175>;
 //            require-prior-idle-ms = <150>;
             bindings = <&kp>, <&kp>;
-            hold-trigger-key-positions = <KEYS_L/* THUMBS*/>;
+            hold-trigger-key-positions = <KEYS_L THUMBS>;
             hold-trigger-on-release;
         };
 
